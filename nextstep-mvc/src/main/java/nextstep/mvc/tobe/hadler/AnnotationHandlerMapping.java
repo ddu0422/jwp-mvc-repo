2ca5def5e39ea3db15e8befb442b551f88c852bc
이두호 <ddu0422@naver.com>
2019-10-09 20:22:26 +0900

    refactor: instance 생성 책임 분리

--- nextstep-mvc/src/main/java/nextstep/mvc/tobe/hadler/AnnotationHandlerMapping.java
+++ nextstep-mvc/src/main/java/nextstep/mvc/tobe/hadler/AnnotationHandlerMapping.java
@@ -7,7 +7,6 @@ import nextstep.web.annotation.RequestMapping;
 import nextstep.web.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Map;
@@ -15,29 +14,22 @@ import java.util.Set;
 
 public class AnnotationHandlerMapping implements HandlerMapping {
     private Object[] basePackage;
+    private ComponentScanner componentScanner;
 
     private Map<HandlerKey, HandlerExecution> handlerExecutions = Maps.newHashMap();
 
     public AnnotationHandlerMapping(Object... basePackage) {
         this.basePackage = basePackage;
+        this.componentScanner = new ComponentScanner(basePackage);
     }
 
     @Override
     public void initialize() {
-        Set<Class<?>> annotatedWithController = new ComponentScanner(basePackage).findController();
+        Set<Class<?>> annotatedWithController = componentScanner.findController();
 
         for (Class<?> clazz : annotatedWithController) {
             Method[] methods = clazz.getDeclaredMethods();
-            checkMethod(createInstance(clazz), methods);
-        }
-    }
-
-    private Object createInstance(Class<?> clazz) {
-        try {
-            Constructor<?> constructor = clazz.getConstructor();
-            return constructor.newInstance();
-        } catch (Exception e) {
-            throw new IllegalArgumentException("class has not no-arg constructor");
+            checkMethod(componentScanner.createInstance(clazz), methods);
         }
     }
 
